11096ddc9e84e933c51dfb79b4898bf68902a122
lucascs10 <lucascs10@gmail.com>
2019-07-22 14:22:01 -0400

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Calculator{
+public class Calculadora{
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 
